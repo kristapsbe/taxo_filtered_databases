8ba878f223b4c0a67b07b3d5e0ca5cf3183545c2
Kristaps <kbebris+git@gmail.com>
2020-07-13 13:47:31 +0300

    added filter

--- generate_mapping.py
+++ generate_mapping.py
@@ -42,5 +42,37 @@ for fname in glob.glob("refseq_prot/refseq/release/*/*.gbff.gz"):
                 # we've found the taxid for the next bunch of sequences - save it
                 mapping[curr_org][l.split(":")[1].split("\"")[0]] = accession_data
 
+with open('orig_mapping.json', 'w') as file:
+     file.write(json.dumps(mapping))
+                
+tmp_mapping = {}
+
+for species,taxids in mapping.items():
+    if os.path.exists("nucl/nucl_"+species):
+        tmp_species = {}
+        
+        with open("nucl/nucl_"+species, "r") as f:
+            r = f.readline()
+            while r:
+                if ">" in r:
+                    currid = r.split(" ")[0].split(".")[0][1:]
+                    
+                    for taxid,accessionids in taxids.items():
+                        if currid in accessionids:
+                            if taxid not in tmp_species:
+                                tmp_species[taxid] = []
+                            
+                            tmp_species[taxid].append(currid)
+                            break # not expecting there to be more than one reference to ir
+
+                r = f.readline()
+                
+        if len(tmp_species) > 0:
+            tmp_mapping[species] = tmp_species
+        else:
+            print(species+" dropped")
+
+mapping = tmp_mapping
+                
 with open('mapping.json', 'w') as file:
      file.write(json.dumps(mapping))
